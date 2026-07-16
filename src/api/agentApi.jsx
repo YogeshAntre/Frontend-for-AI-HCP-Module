9b831254e3afb289a0e925bd018322d07a7a122d
YogeshAntre <yogaantre@gmail.com>
2026-07-16 10:36:47 +0530

    add env files

--- src/api/agentApi.jsx
+++ src/api/agentApi.jsx
@@ -1,19 +1,33 @@
+// import axios from "axios";
+
+// const API="http://127.0.0.1:8000";
+
+
+// export const sendMessage=async(message)=>{
+
+
+//     const response=await axios.post(
+//         `${API}/agent/chat`,
+//         {
+//             message:message
+//         }
+//     );
+
+
+//     return response.data;
+
+// }
 import axios from "axios";
 
-const API="http://127.0.0.1:8000";
+const API = import.meta.env.VITE_API_URL;
 
+export const sendMessage = async (message) => {
+  const response = await axios.post(
+    `${API}/agent/chat`,
+    {
+      message: message,
+    }
+  );
 
-export const sendMessage=async(message)=>{
-
-
-    const response=await axios.post(
-        `${API}/agent/chat`,
-        {
-            message:message
-        }
-    );
-
-
-    return response.data;
-
-}
+  return response.data;
+};
